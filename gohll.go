c1094eb326d9221223e9cb19f6c1861d1d2eb6e0
Albert Visagie <avisagie@gmail.com>
2013-11-04 19:00:31 +0200

    first commit

---
+++ gohll.go
@@ -0,0 +1,127 @@
+// Implementation of HyperLogLog from Flajolet et al, 2007. This is
+// the 32-bit version from the paper only. A similar analysis would be
+// required to get the constants right for 64-bit.
+//
+// This does not supply the 32-bit hash function. Check out hash/fnv
+// from the go standard library.
+//
+package gohll
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	two32 = 1 << 32
+)
+
+type HLL struct {
+	alpha float64
+	b     uint
+	m     uint
+	mask  uint32
+	eM    []uint8
+}
+
+func alpha(b uint) float64 {
+	switch {
+	case b == 4:
+		return 0.673
+	case b == 5:
+		return 0.697
+	case b == 6:
+		return 0.709
+	case b > 6 && b < 17:
+		return 0.7213 / (1 + 1.079/float64(uint(1)<<b))
+	default:
+		panic(fmt.Sprint("Value for b must be in [4,16]:", b))
+	}
+}
+
+// Create a new HLL counter. 2**b is the number of registers. Good
+// values include 4 to 16. Assumes 32-bit hashes which are good for
+// counting sets with up to 1e9 unique items.
+func NewHLL(b uint) *HLL {
+	m := uint(1) << b
+	return &HLL{alpha: alpha(b), b: b, m: m, eM: make([]uint8, m), mask: uint32(0xffffffff) >> b}
+}
+
+// Count leading zeros. From http://en.wikipedia.org/wiki/Find_first_set#Algorithms
+func clz32(x uint32) uint {
+	if x == 0 {
+		return 32
+	}
+	n := uint(0)
+	if (x & 0xFFFF0000) == 0 {
+		n = n + 16
+		x = x << 16
+	}
+	if (x & 0xFF000000) == 0 {
+		n = n + 8
+		x = x << 8
+	}
+	if (x & 0xF0000000) == 0 {
+		n = n + 4
+		x = x << 4
+	}
+	if (x & 0xC0000000) == 0 {
+		n = n + 2
+		x = x << 2
+	}
+	if (x & 0x80000000) == 0 {
+		n = n + 1
+	}
+	return n
+}
+
+// Add the hash value of an item to be counted to the counter.
+func (h *HLL) Add(hashValue uint32) {
+	j := hashValue >> (32 - h.b)
+	emj := h.eM[j]
+	w := uint8(clz32(hashValue&h.mask) - h.b + 1)
+	if emj < w {
+		h.eM[j] = w
+	}
+}
+
+// Calculate the actual count.
+func (h *HLL) Count() uint32 {
+	acc := 0.0
+	for _, eMj := range h.eM {
+		acc += math.Pow(2.0, -float64(eMj))
+	}
+
+	fm := float64(h.m)
+	E := h.alpha * fm * fm / acc
+
+	if E <= 2.5*fm {
+		count := 0
+		for _, x := range h.eM {
+			if x == 0 {
+				count++
+			}
+		}
+		if count == 0 {
+			return uint32(E)
+		}
+		return uint32(fm * math.Log(fm/float64(count)))
+	} else if E < 1/30.0*float64(math.MaxUint32) {
+		return uint32(E)
+	} else {
+		return uint32(-two32 * math.Log(1.0-E/two32))
+	}
+}
+
+// Work out the union between this HLL and another. This one is
+// altered. Their b values must be the same.
+func (h *HLL) Union(other *HLL) {
+	if h.b != other.b {
+		panic(fmt.Sprintf("HLLs must use the same number of bits for buckets: this=%d, other=%d", h.b, other.b))
+	}
+	for i, c := range other.eM {
+		if h.eM[i] < c {
+			h.eM[i] = c
+		}
+	}
+}
---
+++ gohll_test.go
@@ -0,0 +1,96 @@
+package gohll
+
+import (
+	"fmt"
+	"hash/fnv"
+	"math"
+	"math/rand"
+	"testing"
+)
+
+func TestGohll1(t *testing.T) {
+	hll := NewHLL(5)
+	if len(hll.eM) != 32 {
+		t.Fatal("1")
+	}
+
+	hll.Add(1)
+	if hll.eM[0] != 27 {
+		t.Fatal("2", hll.eM)
+	}
+	hll.Add(0)
+	if hll.eM[0] != 28 {
+		t.Fatal("3")
+	}
+
+	x := uint32(17) << 27
+	hll.Add(1 | x)
+	if hll.eM[17] != 27 {
+		t.Fatal("4")
+	}
+	hll.Add(0 | x)
+	if hll.eM[17] != 28 {
+		t.Fatal("5")
+	}
+}
+
+func TestGohll2(t *testing.T) {
+	for N := 10; N <= 10000000; N *= 100 {
+		for b := uint(4); b <= 16; b += 2 {
+			if e := math.Abs(exp(t, b, N)); (b < 10 && e > 30.0) || (b >= 10 && e > 7.0) {
+				t.Fatal("Error out of bounds:", e)
+			}
+		}
+	}
+}
+
+func exp(t *testing.T, b uint, N int) (errorPerc float64) {
+	hll := NewHLL(b)
+	for ii := 0; ii < N; ii++ {
+		hll.Add(rand.Uint32())
+	}
+	C := hll.Count()
+	errorPerc = 100.0 * (float64(N) - float64(C)) / float64(N)
+	t.Logf("b = %d, expect %d, got %d, error %0.02f%%", b, N, C, errorPerc)
+	return
+}
+
+func TestClz(t *testing.T) {
+	x := uint32(0xffffffff)
+	for ii := uint(0); ii < 32; ii++ {
+		if z := clz32(x); z != ii {
+			t.Fatalf("expected %d, clz says %d. x = 0x%08x", ii, z, x)
+		}
+		x = x >> 1
+	}
+}
+
+func add(hll *HLL) {
+	f := fnv.New32()
+	for i := 0; i < 1000; i++ {
+		x := rand.Int31n(10000)
+		f.Write([]byte(fmt.Sprintf("%d%d", x, x)))
+		hll.Add(f.Sum32())
+		f.Reset()
+	}
+}
+
+func TestHLLUnion(t *testing.T) {
+	hll := NewHLL(10)
+
+	add(hll)
+	c := hll.Count()
+	t.Logf("Expect 1000, got %d", c)
+	for i := 0; i < 10; i++ {
+		prev := c
+		add(hll)
+		c := hll.Count()
+		t.Logf("Got %d", c)
+
+		// A slightly simplistic test: at least make sure it
+		// keeps getting bigger.
+		if c < prev {
+			t.Fatalf("Count should not get smaller: %d < %d", c, prev)
+		}
+	}
+}
